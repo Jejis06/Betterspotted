32655801e3d29dfe79929e03d3c2bb645861c90d
Jejis06 <75470597+Jejis06@users.noreply.github.com>
2022-05-25 11:14:39 +0200

    Update setup.py

--- poster/setup.py
+++ poster/setup.py
@@ -1,8 +1,8 @@
 import os
 
-print("[installing modules]")
+print("[installing needed node modules]")
 os.system("npm install tough-cookie-filestore2")
 os.system("npm install instagram-web-api")
 os.system("npm install dotenv --save")
-print("[installing python modules]")
+print("[installing needed python modules]")
 os.system("pip install pillow numpy flask colorama datetime apscheduler")
